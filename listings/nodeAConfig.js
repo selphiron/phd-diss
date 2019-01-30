7d8b1dc4cb64945a30d73edbdbcecd71afcc5e76
MoeweX <Jonathan@Hasenburg.de>
2019-01-30 15:46:40 +0100

    Add initial template version

---
+++ listings/nodeAConfig.js
@@ -0,0 +1,11 @@
+{
+  "nodeID" : "nodeA",
+  "publicKey" : "<public key>",
+  "encryptionAlgorithm" : "RSA",
+  "machines" : [ "192.168.0.132", "192.168.0.165" ],
+  "publisherPort" : 8000,
+  "messagePort" : 8010,
+  "restPort" : 8080,
+  "location" : "52.515249, 13.326476",
+  "description" : "Raspberry Pi Cluster in EN 004"
+}
